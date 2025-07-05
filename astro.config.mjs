10e79e988a1bbab5ecc6eed5d082b06dce06a374
SanSpeen <ktns0930@gmail.com>
2025-07-04 22:37:33 -0500

    Added Server Islands and applied to the specific components to keep the app static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output:'server',
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
+      SCORE_API_ENDPOINT: envField.string({context: 'server', access:'public'})
+    }
+  },
   vite: {
     plugins: [tailwindcss()]
   }
