2a9aaffd11958b789f5773cacbed64334b0cf940
SanSpeen <ktns0930@gmail.com>
2025-07-04 22:44:07 -0500

    Tutorial completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,22 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output:'server',
+
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
       SCORE_API_ENDPOINT: envField.string({context: 'server', access:'public'})
     }
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
